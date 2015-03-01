91b8ff7cbab1bdcac14360eec053c8f0f5059898
Joel Theytaz <j.theytaz@gmail.com>
2015-03-01 18:46:57 +0100

    Add my get_unique_id file

--- get_unique_id.c
+++ get_unique_id.c
@@ -1,6 +1,18 @@
 #include<linux/linkage.h>
+#include<linux/uaccess.h>
+#include<linux/types.h>
 
-asmlinkage long sys_get_unique_id(int *uuid )
+atomic_t counter = ATOMIC_INIT(0);
+
+asmlinkage long sys_get_unique_id(int *uuid)
 {
-    return -1;
+  long result;
+  result = put_user(atomic_read(&counter), uuid);
+    
+  // If the put_user succeeded, we increment the counter.
+  if (result == 0) {
+    atomic_inc(&counter);
+  }
+  
+  return result;
 }
